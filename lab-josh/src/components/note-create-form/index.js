bc470ee71c57b8f752dcf0ce7a25e8a2076e4287
Jozue06 <jozue06@gmail.com>
2018-08-07 21:01:03 -0700

    reset refarctored

--- lab-josh/src/components/note-create-form/index.js
+++ lab-josh/src/components/note-create-form/index.js
@@ -25,14 +25,17 @@ export default class NoteForm extends React.Component {
     e.preventDefault();
     this.props.addNote(this.state);
  
-    document.getElementById('form').reset();
+    this.setState({
+      noteName:'',
+      content:''
+    })
   }
 
 
   render() {
     return <form id="form" onSubmit={this.handleSubmit}>
-     <Style.Text> <input type="text" name="noteName" placeholder="Note Title" onChange={this.handleChange} /></Style.Text> 
-     <Style.Text>  <textarea type="field" name="content" placeholder="text" onChange={this.handleChange} /></Style.Text> 
+     <Style.Text> <input type="text" name="noteName" placeholder="Note Title" value={this.state.noteName} onChange={this.handleChange} /></Style.Text> 
+     <Style.Text>  <textarea type="field" name="content" placeholder="text" value={this.state.content} onChange={this.handleChange} /></Style.Text> 
       <Style.Button type="submit" value="Add">Add Note</Style.Button>
     </form>;
   }
