1faf4f625e78cc89a8b408ae0af1f3d5756e581e
Jozue06 <jozue06@gmail.com>
2018-08-11 15:07:44 -0700

    s

--- src/components/note-update/index.js
+++ src/components/note-update/index.js
@@ -6,29 +6,34 @@ class NoteUpdateForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes: this.props.notes,
+            notes: this.props.notes, 
         }
         this.showUpdateNote = this.showUpdateNote.bind(this);
     }
 
     showUpdateNote() {
         console.log('inside showupdate ', this.props);
-        return <ShowUpdateForm 
-        ident="show update form compo" 
-        save={this.props.save} 
-        id={this.props.id} 
-        notes={this.props.notes} 
-        content={this.props.content} />
+        return <ShowUpdateForm
+            ident="show update form compo"
+            save={this.props.save}
+            id={this.props.id}
+            notes={this.props.notes}
+            content={this.props.content} />
     }
 
 
     render() {
-        console.log('inside update note state', this.props);
-        if (this.props.updatingNote) {
-            return this.showUpdateNote();
+        console.log('HERERERE inside update note state ', this.state);
+        this.state.notes.map(e => {
+            console.log('element . id =', e.id, 'note . id =', this.props.id)
+            if (e.id === this.props.id) {
+                return this.showUpdateNote();
+            }
         }
+        )
+
         return <div>
-            <h4></h4>
+            <h4>d</h4>
         </div>
     }
 }
